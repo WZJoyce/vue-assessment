1708eaa43f6601b14a17ff0c16ef570267fd86e8
weimizhoujoyce@gmail.com <weimizhoujoyce@gmail.com>
2021-03-23 12:30:57 +1030

    first challenge

--- src/views/Challenge1.vue
+++ src/views/Challenge1.vue
@@ -2,15 +2,18 @@
   <div class="challenge">
     <h1>Bug Challenge 1</h1>
     <p>{{ msg }}</p>
+    
   </div>
 </template>
 
 <script>
   export default {
     name: 'Challenge1',
-    data: {
+    data() {
+      return{
       msg: 'Congrats you have discovered the issue',
       msg2: 'Don\t forget to commit this solutiom'
+      }
     }
   }
 </script>
